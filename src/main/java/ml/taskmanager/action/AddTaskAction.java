b5b9431ca61256aeeb3171245aaf220220aaec68
Bankass77 <55981898+Bankass77@users.noreply.github.com>
2023-04-27 19:50:13 +0200

    Update AddTaskAction.java

--- src/main/java/ml/taskmanager/action/AddTaskAction.java
+++ src/main/java/ml/taskmanager/action/AddTaskAction.java
@@ -14,7 +14,7 @@ import ml.taskmanager.services.TaskService;
 
 @RequiredArgsConstructor
 @AllArgsConstructor
-public class AddTaskAction extends ActionSupport {
+public class TaskAction extends ActionSupport {
 
     private static final long serialVersionUID = 1L;
 	private TaskService taskService;
@@ -65,4 +65,99 @@ public class AddTaskAction extends ActionSupport {
     public void setCompleted(boolean completed) {
         this.completed = completed;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private Task task;
+    // getters and setters for task property
+    
+    
+
+
+
+
+
+
+    
+    public String execute() throws Exception {
+        try (Connection conn = getConnection()) {
+            String sql = "SELECT * FROM tasks WHERE id = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, task.getId());
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                task.setName(rs.getString("name"));
+                task.setDescription(rs.getString("description"));
+                task.setDueDate(rs.getDate("due_date"));
+            }
+        }
+        return SUCCESS;
+    }
+    
+    public String create() throws Exception {
+        try (Connection conn = getConnection()) {
+            String sql = "INSERT INTO tasks (name, description, due_date) VALUES (?, ?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, task.getName());
+            stmt.setString(2, task.getDescription());
+            stmt.setDate(3, new java.sql.Date(task.getDueDate().getTime()));
+            stmt.executeUpdate();
+        }
+        return SUCCESS;
+    }
+    
+    public String update() throws Exception {
+        try (Connection conn = getConnection()) {
+            String sql = "UPDATE tasks SET name = ?, description = ?, due_date = ? WHERE id = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, task.getName());
+            stmt.setString(2, task.getDescription());
+            stmt.setDate(3, new java.sql.Date(task.getDueDate().getTime()));
+            stmt.setInt(4, task.getId());
+            stmt.executeUpdate();
+        }
+        return SUCCESS;
+    }
+    
+    public String delete() throws Exception {
+        try (Connection conn = getConnection()) {
+            String sql = "DELETE FROM tasks WHERE id = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, task.getId());
+            stmt.executeUpdate();
+        }
+        return SUCCESS;
+    }
+    
+    public List<Task> getTasks() throws Exception {
+        List<Task> tasks = new ArrayList<>();
+        try (Connection conn = getConnection()) {
+            String sql = "SELECT * FROM tasks";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Task task = new Task();
+                task.setId(rs.getInt("id"));
+                task.setName(rs.getString("name"));
+                task.setDescription(rs.getString("description"));
+                task.setDueDate(rs.getDate("due_date"));
+                tasks.add(task);
+            }
+        }
+        return tasks;
+    }
+}
+
 }
